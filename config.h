23a4f5c568f2d1fda7a02733d50c5944669a613f
Isac <91141474+Isac-AS@users.noreply.github.com>
2023-08-08 22:50:04 +0100

    changed font

--- config.h
+++ config.h
@@ -4,10 +4,11 @@
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 static const unsigned int alpha = 220;     /* Amount of opacity. 0xff is opaque             */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 700;                    /* minimum width when centered */
+static int min_width = 1000;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	//"monospace:size=10"
+	"hack:size=15"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
